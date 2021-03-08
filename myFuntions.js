fe6de0c2876f9f6c246b5becaab0edda945913d4
Freitez93 <46011203+Freitez93@users.noreply.github.com>
2021-03-07 22:59:45 -0400

    Update and rename code.js to myFuntions.js

---
+++ myFuntions.js
@@ -0,0 +1,99 @@
+let bypassed = false,
+	navigated = false,
+	isGoodLink_allowSelf = false,
+	referer = window.location.href,
+	hostName = window.location.hostname;
+const URL = window.URL,
+	getRandom = (min, max) => Math.floor(Math.random() * (max - min)) + min,
+	getElementByXpath = path => document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue,
+	parseTarget = target => target instanceof HTMLAnchorElement ? target.href : target,
+	unsafelyAssignWithReferer = (target, referer) => {
+		//The background script will intercept this request and handle it.
+		location.href="https://universal-bypass.org/navigate?target="+encodeURIComponent(target)+"&referer="+encodeURIComponent(referer)
+	},
+	unsafelyAssign = target => {
+		navigated = true
+		window.onbeforeunload = null
+		location.assign(target)
+	},
+	safelyAssign = target => {
+		target = parseTarget(target)
+		if (navigated || !isGoodLink(target)) return false;
+
+		bypassed = true
+		let url = new URL(target)
+		if (!url || !url.hash) target += location.hash;
+
+		unsafelyAssign(target)
+		return true
+	},
+	isGoodLink = link => {
+		if (typeof link != "string" || (link.split("#")[0] == referer.split("#")[0] && !isGoodLink_allowSelf) || link.substr(0, 6) == "about:" || link.substr(0, 11) == "javascript:") {
+			return false
+		}
+		try {
+			new URL(link)
+		} catch (e) {
+			return false
+		}
+		return true
+	},
+	awaitElement = (element, f) => {
+		let t = setInterval(() => {
+			let e = document.querySelector(element)
+			if (e) {
+				f(e), clearInterval(t)
+			}
+		}, 600)
+		setInterval(() => clearInterval(t), 60000)
+	},
+	ifElement = (q, f, ef) => ensureDomLoaded(() => {
+		let e = document.querySelector(q)
+		if (e) f(e);
+		else if (ef) ef();
+	}),
+	hrefBypass = (regex, f) => {
+		if (bypassed) return;
+		if (typeof f != "function") alert("AdsBypasser: Bypass for " + domain + " is not a function");
+
+		let res = regex.exec(window.location.href)
+		if (res) {
+			window.document.title += ' - AdsBypasser';
+			bypassed = true;
+			f(res)
+		}
+	},
+	domainBypass = (domain, f) => ensureDomLoaded(() => {
+		if (bypassed) return;
+		if (typeof f != "function") alert("AdsBypasser: Bypass for " + domain + " is not a function");
+
+		if (typeof domain == "string") {
+			if (hostName == domain || hostName.substr(hostName.length - (domain.length + 1)) == "." + domain) {
+				window.document.title += ' - AdsBypasser';
+				bypassed = true
+				f()
+			}
+		} else if ("test" in domain) {
+			if (domain.test(hostName)) {
+				window.document.title += ' - AdsBypasser';
+				bypassed = true
+				f()
+			}
+		} else {
+			console.error("[AdsBypasser] Invalid domain:", domain)
+		}
+	}),
+	ensureDomLoaded = (f, if_not_bypassed) => {
+		if (if_not_bypassed && bypassed) return;
+		if (["interactive", "complete"].indexOf(document.readyState) > -1) {
+			f()
+		} else {
+			let triggered = false
+			document.addEventListener("DOMContentLoaded", () => {
+				if (!triggered) {
+					triggered = true
+					setTimeout(f, 1)
+				}
+			})
+		}
+	};

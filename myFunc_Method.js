a0b345e0c0515dc5b90a8e65d9e30d3fba7cac9b
Freitez93 <46011203+Freitez93@users.noreply.github.com>
2021-03-24 17:58:02 -0400

    Update myFunc_Method.js

--- myFunc_Method.js
+++ myFunc_Method.js
@@ -117,46 +117,31 @@ const myFunc = {
 			})
 		}
 	},
-	awaitState: (event, callback) => {
-		var jQuery = jQuery.fn.jquery;
+	onReady: callback => {
+		var version = jQuery.fn.jquery;
+		var readyFired = false;
+		var ready = function(){
+			if ( !readyFired ){
+				readyFired = true;
+				setTimeout(callback, 100)
+			}
+		}
 
 		if (typeof $ === 'function') {
-			if (jQuery.split('.')[0] == '3') {
-				$(window).on('load', callback)
+			if (version.split('.')[0] == '3') {
+				$(window).on('load', ready)
 			} else {
-				$(window).load(callback)
+				$(window).load(ready)
 			}
 		} else {
-			if (['loading', 'interactive', 'complete'].indexOf(event) == -1) console.error('Event desconocido.')
-			let loop = setInterval(function() {
-				if (document.readyState == event) {
-					clearInterval(loop)
-					callback()
-				}
-			}, 500)
-		}
-	},
-	docReady: (callback, context) => {
-		// si el documento ya está listo, programe la función callback para que se ejecute
-		// IE solo es seguro cuando readyState está "complete", otros seguros cuando readyState es "interactive"
-		if (document.readyState === 'complete' || (!document.attachEvent && document.readyState === 'interactive')) {
-			setTimeout(callback, 1);
-		} else {
-			// de lo contrario, si no tenemos controladores de eventos instalados, instálelos
-			if (document.addEventListener) {
-				// la primera opción es el evento DOMContentLoaded
-				document.addEventListener("DOMContentLoaded", function(){
-					// copia de seguridad es el evento de window load
-					window.addEventListener("load", callback, false);
-				}, false);
+			if (document.readyState === 'complete') {
+				ready()
 			} else {
-				// debe ser IE
-				document.attachEvent("onreadystatechange", function(){
-					if (document.readyState === 'complete'){
-						setTimeout(callback, 1);
-					}
-				});
-				window.attachEvent("onload", callback);
+				// la primera opción es el evento DOMContentLoaded
+				document.addEventListener('DOMContentLoaded', ready, false)
+
+				// por si el primer evento no funciona seguimos con window load
+				window.addEventListener('load', ready, false)
 			}
 		}
 	},

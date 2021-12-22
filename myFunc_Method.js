15c00acc52004aec538f000cb2263a9d51807bd9
Freitez93 <46011203+Freitez93@users.noreply.github.com>
2021-12-22 16:51:25 -0400

    Update myFunc_Method.js

--- myFunc_Method.js
+++ myFunc_Method.js
@@ -74,8 +74,7 @@ const myFunc = {
 				callback(element);
 			}
 			repeat = (repeat) ? repeat - 1 : (
-				console.log('[awaitElement] Error en esperar a que aparesca el element.')
-				clearInterval(loop)
+				console.log('[awaitElement] Error en esperar a que aparesca el element.'), clearInterval(loop)
 			)
 		}, 1000);
 	},
